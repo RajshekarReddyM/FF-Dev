ccf08b324e675111356c0901be1e9ed0065a6aa2
RajshekarReddyM <rajashekarreddy0797@gmail.com>
2023-02-27 11:51:44 +0530

    hh

---
+++ React-JS/js-mastery/src/index.js
@@ -0,0 +1,7 @@
+// import ReactDOM from 'react-dom'
+
+// ReactDOM.render("Hello Raj", document.getElementById("raj"))
+
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(document.getElementById("raj"));
